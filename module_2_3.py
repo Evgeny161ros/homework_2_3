097fe32b6be1d730104984769d1bd39660e0623b
EvgenyShishkin <rostow.papa.2015@gmail.com>
2024-09-05 17:35:50 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,13 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+while index < len(my_list):
+    x = my_list[index]
+    index = index + 1
+    if x == 0:
+        continue
+    elif x < 0:
+        break
+    elif index == len(my_list):
+        print(x)
+    else:
+        print(x)
